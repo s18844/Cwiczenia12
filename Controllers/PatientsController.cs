8e9ee73407ddaf7e1b3c8c8fecaca6e0d1abc352
s18844 <61825554+s18844@users.noreply.github.com>
2020-06-03 14:29:59 +0200

    Initial commit

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenia12.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia12.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly s18844Context _context;
+        public PatientsController(s18844Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var db = new s18844Context();
+            var patients = db.Patient.ToList();
+            return View(patients);
+        }
+
+        public string GetDoctors()
+        {
+            return "AKA";
+        }
+
+        [HttpPost]
+        public IActionResult Create(Patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(patient);
+            }
+            var db = new s18844Context();
+            db.Patient.Add(patient);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/Studies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cwiczenia12.Models
+{
+    public partial class Studies
+    {
+        public int IdStudy { get; set; }
+        public string Name { get; set; }
+    }
+}
